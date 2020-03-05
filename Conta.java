414025808b26daee1cbd852e5193db79ed06f41e
Ricardo Baccarini <ricardo.baccarini@gmail.com>
2020-03-05 20:51:01 -0300

    pacote unico

--- Conta.java
+++ Conta.java
@@ -1,29 +1,91 @@
 class Conta {
-  String titular;
-  int numero;
-  String agencia;
-  double saldo;
-  String dataDeAbertura;
+  private String titular;
+  private int numero;
+  private String agencia;
+  private Double saldo;
+  private String dataDeAbertura;
+  private int contador;
+  private static int indentificador; 
+  
+  Conta() {
+    indentificador ++;
+    contador = indentificador;
+  }
 
-  void saca(double valor){
-    saldo -= valor;
+  Conta(String titular){
+    indentificador ++;
+    contador = indentificador;
+    this.setTitular(titular);
+  }
+
+        public int getContador()
+	{
+	  return contador;
+	}
+ 
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+  public String getDataDeAbertura(){
+    return dataDeAbertura;
   }
   
-  void deposita(double valor){
-  	saldo += valor;
+  public void setDataDeAbertura(String data){
+    this.dataDeAbertura = data;
+  }
+    
+  public void saca(double valor){
+    this.saldo -= valor;
   }
   
-  double calculaRendimento(){
-    double rendimento = saldo * 0.1;
-    saldo += rendimento;
-    return saldo;
+  public void deposita(double valor){
+  	this.saldo += valor;
   }
   
-  String recuperaDadosParaImpressao(){
-    String dados = "Titular " + this.titular;
-    dados += "\nNúmero: " + this.numero + " agência " + this.agencia;
-    dados += "\nData de abetura " + this.dataDeAbertura;
-    dados += "\nSaldo " + this.saldo; 
+  public double calculaRendimento(){
+    double rendimento = this.getSaldo() * 0.1;
+    this.saldo += rendimento;
+    return getSaldo();
+  }
+  
+  public String recuperaDadosParaImpressao(){
+    String dados = "Titular " + this.getTitular();
+    dados += "\nNúmero: " + this.getNumero() + " agência " + this.getAgencia();
+    dados += "\nData de abetura " + this.getDataDeAbertura();
+    dados += "\nSaldo " + this.getSaldo(); 
     return dados; 
+  }
+
+  public void transfere(double valor, Conta destino){
+    this.saldo -= valor;
+    destino.saldo += valor;
   }
 }
--- UsaConta.java
+++ UsaConta.java
@@ -1,17 +1,28 @@
 class UsaConta{
   public static void main(String [] args){
   	Conta c1 = new Conta();
-  	c1.titular = "Mané";
-  	c1.numero = 98082;
-  	c1.agencia = "333";
-  	c1.saldo = 50.99;
-  	c1.dataDeAbertura = "04/03/2020";
-  	System.out.println(c1.titular + " saldo " + c1.saldo);
+  	c1.setTitular("Mané");
+  	c1.setNumero(98082);
+  	c1.setAgencia("333");
+  	c1.setSaldo(50.99);
+  	c1.setDataDeAbertura("04/03/2020");
+  	System.out.println(c1.getTitular() + " saldo " + c1.getSaldo());
   	c1.deposita(153.85);
-  	System.out.println(c1.titular + " saldo " + c1.saldo);
+  	System.out.println(c1.getTitular() + " saldo " + c1.getSaldo());
   	c1.calculaRendimento();
   	System.out.println(c1.recuperaDadosParaImpressao());
   	
+        System.out.println("Identificador " +  c1.getContador());
+	Conta cIdentificador = new Conta("ricardo");
+        System.out.println("Identificador " +  cIdentificador.getContador());
+        System.out.println("Identificador " +  c1.getContador());
+
+  	System.out.println("endereco da variavel de referencia c1  ");
+  	System.out.println(c1);
+  	
+  	//Comentario
+  	/*
+  	System.out.println("");
   	Conta c2 = new Conta();
   	c2.titular = "Ricardo";
   	c2.saldo = 90;
@@ -20,6 +31,7 @@ class UsaConta{
   	c3.titular = "Ricardo";
   	c3.saldo = 90;
   	
+  	System.out.println("Verificando igualdade entre c1 e c2");
   	if (c1 == c2){
   		System.out.println("iguais");
   	} else {
@@ -30,11 +42,27 @@ class UsaConta{
   	c3.titular = "ERERE";
   	c3.saldo = 90;
   	
+  	System.out.println("Saldo c2 " + c2.saldo + " saldo c3 " + c3.saldo);
+  	c2.transfere(100, c3);
+  	System.out.println("Saldo c2 " + c2.saldo + " saldo c3 " + c3.saldo);
+
+  	System.out.println("Verificando igualdade entre c2 e c3");
   	c2 = c3;
   	if (c1 == c2){
   		System.out.println("iguais");
   	} else {
   	  System.out.println("diferentes");
   	}
+
+		if (c2.equals(c3)) {
+		  System.out.println("Equals");
+		}
+		*/
+		
+/*
+		Conta novoConstrutor = new Conta("fumaca");
+		System.out.println(novoConstrutor.getTitular());
+
+*/
   }
 }
