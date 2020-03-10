6968f19e9c0b5b1e0bfa46c48eb71f8ec621c6e6
Ricardo Baccarini <ricardo.baccarini@gmail.com>
2020-03-10 18:49:49 -0300

    Implementado a classe tributavel

--- fj11-contas/src/br/com/caelum/contas/modelo/ContaCorrente.java
+++ fj11-contas/src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -1,6 +1,6 @@
 package br.com.caelum.contas.modelo;
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 	@Override
 	public void saca(double valor) {
 		this.saldo -= (valor + 0.10);
@@ -8,6 +8,12 @@ public class ContaCorrente extends Conta {
 
 	public String getTipo() {
 		return "Conta Corrente";
+	}
+
+	@Override
+	public double getValorImposto() {
+		// TODO Auto-generated method stub
+		return this.getSaldo() * 0.01;
 	}
 
 }
